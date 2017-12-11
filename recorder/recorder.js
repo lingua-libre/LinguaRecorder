fe86d7442a4b0e98e5c028f8356e388559bfdd8a
Antoine Lamielle <gh@0x010c.fr>
2017-12-11 22:10:39 +0100

    [recorder] use the new buffer system and add autoStart/autoStop feature

--- recorder/recorder.js
+++ recorder/recorder.js
@@ -1,16 +1,38 @@
+'use strict';
+
+const STATE = {
+    stop: 'stop',
+    listening: 'listening',
+    recording: 'recording',
+    paused: 'paused',
+}
+
+/*
+TODO
+- Audio class
+  - raw to download
+  - raw to wav
+  - raw to html5
+- saturation control
+- buffer size
+- time limit
+- GUI
+- clean useless config
+- rtrim on autostop
+- documentation
+*/
+
 /**
  *
  *
  * @constructor
  * @param {Object} [config] Configuration options
- * @cfg {boolean} [saturationControl] Whether the recorder should discard the record when it saturate
  * @cfg {boolean} [autoStart]
  * @cfg {boolean} [autoStop]
+ * @cfg {boolean} [autoRestart]
  * @cfg {integer} [bufferSize]
  * @cfg {integer} [timeLimit]
- * @cfg {boolean} [encodeAfterRecord]
- * @cfg {integer} [progressInterval] (in millisec)
- * @cfg {} []
+ * @cfg {boolean} [saturationControl] Whether the recorder should discard the record when it saturate
  * @cfg {} []
  * @cfg {} []
  */
@@ -19,12 +41,28 @@ var Recorder = function( config ) {
 	config = config || {};
 
 	this.stream = null;
-	this._recording = false;
-	this._paused = false;
+
+    this.autoStart = false || config.autoStart;
+    this.autoStop = false || config.autoStop;
+    this.autoRestart = false || config.autoRestart;
+    this.bufferSize = 4096 || config.bufferSize;
+    this.timeLimit = null || config.timeLimit;
+	this.saturationControl = 0.99 || config.saturationControl;
+	this.startThreshold = 0.1 || config.startThreshold;
+	this.silenceThreshold = 0.05 || config.silenceThreshold;
+	this.bufferLength = 0.8 || config.bufferLength;
+	this.marginBefore = 0.5 || config.marginBefore;
+	this.marginAfter = 0.25 || config.marginAfter;
+	this.stopSilence = 0.3 || config.stopSilence;
+	this.minimalLength = 0.15 || config.minimalLength;
+
+	this._state = STATE.stop;
+
 	this._eventListeners = {
 		ready: [],
 		readyFail: [],
 		started: [],
+		listening: [],
 		recording: [],
 		paused: [],
 		stoped: [],
@@ -35,6 +73,7 @@ var Recorder = function( config ) {
 		ready: null,
 		readyFail: null,
 	};
+
 
 	this._getAudioStream();
 };
@@ -51,8 +90,8 @@ Recorder.prototype.getRecordingTime = function() {
 
 };
 
-Recorder.prototype.getSamples = function() {
-
+Recorder.prototype.getAudioBuffer = function() {
+    return this.rawAudioBuffer;
 };
 
 Recorder.prototype.getEncodedRecord = function() {
@@ -60,68 +99,81 @@ Recorder.prototype.getEncodedRecord = function() {
 };
 
 Recorder.prototype.start = function() {
-	if ( this.node === undefined || this._recording ) {
+	if ( this.audioContext === undefined || this._state in [ STATE.listening, STATE.recording ] ) {
 	    return false;
 	}
 
-	if ( ! this._paused ) {
-		this._clear();
-	}
-	this._recording = true;
-	this._paused = false;
-	this.audioInput.connect(this.node);
-	this.node.connect(this.audioContext.destination);
+	if ( this._state === STATE.stop ) {
+		this._initAttr();
 
-	this.fire( 'started' );
+        if ( this.autoStart ) {
+            this._state = STATE.listening;
+            this._connect();
+
+            return true;
+        }
+	}
+
+    this._state = STATE.recording;
+    this._connect();
+
+    this.fire( 'started' );
+
 	return true;
 };
 
 Recorder.prototype.pause = function() {
-    if ( ! this._recording ) {
+    if ( this._state !== STATE.recording ) {
         return false;
     }
 
-	this.audioInput.disconnect( this.node );
-	this.node.disconnect( this.audioContext.destination );
-	this._recording = false;
-	this._paused = true;
+	this._disconnect();
+	if ( this._state === STATE.listening ) {
+	    this._state = STATE.stop;
+	}
+	else {
+	    this._state = STATE.paused;
+	}
 
 	this.fire( 'paused' );
 	return true;
 };
 
 Recorder.prototype.stop = function() {
-    if ( ! this._recording && ! this._paused ) {
+    if ( this._state === STATE.stop ) {
         return false;
     }
 
-    if ( ! this._paused ) {
-	    this.audioInput.disconnect( this.node );
-	    this.node.disconnect( this.audioContext.destination );
-	    this._recording = false;
+    if ( this._state !== STATE.paused ) {
+	    this._disconnect();
 	}
-	this._paused = false;
+	this._state = STATE.stop
 
-	this.fire( 'stoped' );
+	this.fire( 'stoped', this.rawAudioBuffer );
+
+	if ( this.autoRestart ) {
+	    this.start();
+	}
+
 	return true;
 };
 
 Recorder.prototype.cancel = function() {
-    if ( ! this._recording && ! this._paused ) {
+    if ( this._state === STATE.stop ) {
         return false;
     }
 
-	this.audioInput.disconnect( this.node );
-	this.node.disconnect( this.audioContext.destination );
-	this._recording = false;
-	this._paused = false;
+    if ( this._state !== STATE.paused ) {
+	    this._disconnect();
+	}
+	this._state = STATE.stop
 
 	this.fire( 'canceled' );
 	return true;
 };
 
 Recorder.prototype.toggle = function() {
-	if ( this._recording ) {
+	if ( this._state in [ STATE.recording, STATE.listening ] ) {
 		this.stop();
 	}
 	else {
@@ -206,34 +258,118 @@ Recorder.prototype._getAudioStream = function() {
 };
 
 Recorder.prototype._initStream = function() {
+	var recorder = this;
+
     this.audioContext = new window.AudioContext();
 	this.audioInput = this.audioContext.createMediaStreamSource( this.stream );
-	this.buffers = new Buffers();
-	this.buffers.setSamplerate( this.audioContext.sampleRate );
-    console.log(this.bufferLen);
-	this.node = this.audioContext.createScriptProcessor(this.bufferLen, 1, 1);
 
-	var recorder = this;
-	this.node.onaudioprocess = function(e) {
-		recorder._onaudioprocess(e);
+	this._initAttr();
+
+	this.listeningProcessor = this.audioContext.createScriptProcessor( this.bufferLen, 1, 1 );
+	this.listeningProcessor.onaudioprocess = function( e ) {
+		recorder._audioListeningProcess( e );
+	};
+
+	this.recordingProcessor = this.audioContext.createScriptProcessor( this.bufferLen, 1, 1 );
+	this.recordingProcessor.onaudioprocess = function( e ) {
+		recorder._audioRecordingProcess( e );
 	};
 };
 
-Recorder.prototype._onaudioprocess = function(e) {
-	if ( ! this._recording || this._paused ) {
+Recorder.prototype._connect = function() {
+    var processor;
+    if ( this._state === STATE.listening ) {
+        processor = this.listeningProcessor;
+    }
+    else {
+        processor = this.recordingProcessor;
+    }
+
+    this.audioInput.connect( processor );
+    processor.connect( this.audioContext.destination );
+}
+
+Recorder.prototype._disconnect = function() {
+    var processor;
+    if ( this._state === STATE.listening ) {
+        processor = this.listeningProcessor;
+    }
+    else {
+        processor = this.recordingProcessor;
+    }
+
+	this.audioInput.disconnect( processor );
+	processor.disconnect( this.audioContext.destination );
+}
+
+Recorder.prototype._audioListeningProcess = function( e ) {
+    // Discard extra samples if the recording has already been paused or stopped
+	if ( this._state in [ STATE.stop, STATE.paused ] ) {
 	    return;
 	}
 
-	//Copy the samples in a new Float32Array, to avoid memory dealocation
-	var samples = new Float32Array(e.inputBuffer.getChannelData(0));
-	var buffer = new Buffer(samples);
-	this.buffers.push(buffer);
-	console.log( this.buffers.getLength() ); //TODO
-	this.fire( 'recording', buffer );
+	// Get the samples from the input buffer
+	var samples = new Float32Array( e.inputBuffer.getChannelData( 0 ) ); // Copy the samples in a new Float32Array, to avoid memory dealocation
+
+    // Analyse the sound to autoStart when it should
+    for ( var i=0; i < samples.length; i++ ) {
+        var amplitude = Math.abs( samples[ i ] );
+        if ( amplitude > this.startThreshold ) {
+            // start the record
+            this._disconnect();
+            this._state = STATE.recording;
+            this._connect();
+	        this.fire( 'started' );
+            return this._audioRecordingProcess( e );
+        }
+    }
+
+	// Store the sound in the RawAudioBuffer
+    if ( this.marginBefore > 0 ) {
+        this.rawAudioBuffer.push( samples, this.marginBefore );
+    }
+    this.fire( 'listening', samples );
 };
 
-Recorder.prototype._clear = function() {
-	this.buffers.clear();
+Recorder.prototype._audioRecordingProcess = function( e ) {
+    // Discard extra samples if the recording has already been paused or stopped
+	if ( this._state in [ STATE.stop, STATE.paused ] ) {
+	    return;
+	}
+
+	// Get the samples from the input buffer
+	var samples = new Float32Array( e.inputBuffer.getChannelData( 0 ) ); // Copy the samples in a new Float32Array, to avoid memory dealocation
+
+	// Store the sound in the RawAudioBuffer
+    this.rawAudioBuffer.push( samples );
+    this.fire( 'recording', samples );
+
+    // Analyse the sound to autoStop if needed
+    if ( this.autoStop ) {
+        var amplitudeMax = 0;
+        for ( var i=0; i < samples.length; i++ ) {
+            var amplitude = Math.abs( samples[ i ] );
+            if ( amplitude > amplitudeMax ) {
+                amplitudeMax = amplitude;
+            }
+        }
+
+        if ( amplitudeMax < this.silenceThreshold ) {
+            this._silenceSamplesCount += samples.length;
+
+            if ( this._silenceSamplesCount >= ( this.stopSilence * this.audioContext.sampleRate ) ) {
+                this.stop();
+            }
+        }
+        else {
+	        this._silenceSamplesCount = 0;
+        }
+    }
+};
+
+Recorder.prototype._initAttr = function() {
+	this.rawAudioBuffer = new RawAudioBuffer( this.audioContext.sampleRate );
+	this._silenceSamplesCount = 0;
 };
 
 
