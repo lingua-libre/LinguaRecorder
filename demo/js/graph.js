6f31b772e107cc3b6be4b0e365349e7b0c813624
Antoine Lamielle <gh@0x010c.fr>
2018-01-30 18:03:19 +0000

    [demo] release a new demo to show all the features at once

---
+++ demo/js/graph.js
@@ -0,0 +1,62 @@
+const SMOOTHING = 0.8;
+const FFT_SIZE = 512;
+
+
+var Graph = function( canvas, analyser ) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext('2d');
+    this.freqs = new Uint8Array( FFT_SIZE/2 );
+    this.times = new Uint8Array( FFT_SIZE/2 );
+
+    this.setAnalyser( analyser );
+
+    requestAnimationFrame(this.draw.bind(this));
+};
+
+
+Graph.prototype.setAnalyser = function( analyser ) {
+    this.analyser = analyser;
+    this.analyser.minDecibels = -122;
+    this.analyser.maxDecibels = 0;
+    this.analyser.smoothingTimeConstant = SMOOTHING;
+    this.analyser.fftSize = FFT_SIZE;
+};
+
+
+Graph.prototype.draw = function() {
+    // Get the frequency data from the stream
+    this.analyser.getByteFrequencyData(this.freqs);
+    this.analyser.getByteTimeDomainData(this.times);
+
+    // Clear the current content of the canvas
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    // We increase artificially the bar width (times 1.3) to give more space
+    // to visibles bars (the last bars are always very close to 0)
+    this.barWidth = this.canvas.width*1.3 / ( FFT_SIZE / 2 );
+
+    // Draw the frequency domain chart
+    for ( var i = 0; i < this.analyser.frequencyBinCount; i++ ) {
+        var value = this.freqs[i];
+        var percent = value / 256;
+        var height = this.canvas.height * percent;
+        var offset = ( this.canvas.height - height );
+        var hue = i/this.analyser.frequencyBinCount * 360;
+        this.ctx.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
+        this.ctx.fillRect(i * this.barWidth, offset, this.barWidth, height);
+    }
+
+    // Draw the time domain chart
+    this.ctx.fillStyle = 'black';
+    for ( var i = 0; i < this.analyser.frequencyBinCount; i++ ) {
+        var value = this.times[i];
+        var percent = value / 256;
+        var height = this.canvas.height * percent;
+        var offset = this.canvas.height - height - 1;
+        this.ctx.fillRect(i * this.barWidth, offset, this.barWidth, 4);
+    }
+
+    // Ask the browser to callback this function at its next refresh
+    requestAnimationFrame(this.draw.bind(this));
+}
+
---
+++ demo/js/sandbox.js
@@ -0,0 +1,120 @@
+'use strict';
+
+var recorder = null,
+    graph = null;
+
+
+function initRecorder() {
+    recorder = new LinguaRecorder( {
+        autoStart: $("#autoStart").is(':checked'),
+        autoStop: $("#autoStop").is(':checked'),
+        bufferSize: parseInt($("#bufferSize").val()),
+        timeLimit: parseFloat($("#timeLimit").val()),
+        onSaturate: $("#onSaturate").val(),
+        saturationThreshold: parseFloat($("#saturationThreshold").val()),
+        startThreshold: parseFloat($("#startThreshold").val()),
+        stopThreshold: parseFloat($("#stopThreshold").val()),
+        stopDuration: parseFloat($("#stopDuration").val()),
+        marginBefore: parseFloat($("#marginBefore").val()),
+        marginAfter: parseFloat($("#marginAfter").val()),
+        minDuration: parseFloat($("#minDuration").val()),
+    } );
+
+    recorder.on( 'ready', function() {
+        /* Setup the graph with an analyser binded to our new recorder */
+        var analyser = recorder.getAudioContext().createAnalyser();
+        recorder.connectAudioNode( analyser );
+        if ( graph === null ) {
+            graph = new Graph( $( '#canvas' )[0], analyser );
+        }
+        else {
+            graph.setAnalyser( analyser );
+        }
+        incrementEvent( '#ready' );
+    } ).on( 'readyFail', function() {
+        incrementEvent( '#readyfail' );
+    } ).on( 'started', function() {
+        incrementEvent( '#started' );
+    } ).on( 'recording', function() {
+        incrementEvent( '#recording' );
+    } ).on( 'listening', function() {
+        incrementEvent( '#listening' );
+    } ).on( 'saturated', function() {
+        incrementEvent( '#saturated' );
+    } ).on( 'paused', function() {
+        incrementEvent( '#paused' );
+    } ).on( 'stoped', function( audioRecord ) {
+        addSound( audioRecord );
+        incrementEvent( '#stoped' );
+    } ).on( 'canceled', function() {
+        incrementEvent( '#canceled' );
+    } );
+
+}
+
+
+/* Some helper functions */
+function incrementEvent( selector ) {
+  var value = parseInt( $( selector ).text() );
+  $( selector ).text( value + 1 );
+};
+
+
+function addSound( audioRecord ) {
+    /* Generate a nice view */
+    var item = $( '<div class="item">' ),
+        soundIcon = $( '<i class="sound icon">' ),
+        content = $( '<div class="content">' ),
+        header = $( '<div class="header">' ),
+        playLink = $( '<a href="#">' ),
+        playIcon = $( '<i class="volume up icon">' ),
+        downloadLink = $( '<a href="#">' ),
+        downloadIcon = $( '<i class="download icon">' );
+
+    playLink.text( 'play' ).prepend( playIcon );
+    downloadLink.text( 'download' ).prepend( downloadIcon );
+    content.append( header ).append( playLink ).append( downloadLink );
+    item.append( soundIcon ).append( content );
+    $( '#sounds' ).append( item );
+
+    /* Complete with informations from the new AudioRecord */
+    var duration = Math.round( audioRecord.getDuration() * 1000 ) / 1000;
+    header.text( duration + ' seconds' );
+    playLink.click( function( e ) {
+        e.preventDefault();
+        audioRecord.play();
+    } );
+    downloadLink.click( function( e ) {
+        e.preventDefault();
+        audioRecord.download();
+    } );
+
+};
+
+
+
+$( function() {
+
+    /* Init the recorder a first time at startup */
+    initRecorder();
+    /* But re-init it when the refresh button is clicked */
+    $( '#refresh' ).click( function() {
+        recorder.stop();
+        initRecorder();
+    } );
+
+
+    /* Bind events to the buttons to start/pause/stop when asked by the user */
+    $( '#start' ).click( function() {
+        recorder.start();
+    } );
+
+    $( '#pause' ).click( function() {
+        recorder.pause();
+    } );
+
+    $( '#stop' ).click( function() {
+        recorder.stop();
+    } );
+
+} );
